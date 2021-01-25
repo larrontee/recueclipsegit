6a1f9496c7c9609e40e14155c4b7d446674ea726
larrontee <71692653+larrontee@users.noreply.github.com>
2021-01-25 13:11:50 +0100

    Update HelloWorld.java

--- recuperacion/src/recuperacion/HelloWorld.java
+++ recuperacion/src/recuperacion/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	// Imprime "Hola Mundo" a la ventana de la terminal.
 public static void main(String args[])
 	    {
-	        System.out.println("Hola Mundo");
+	        System.out.println("Hola Mundo desde GitHub");
 	    }
 	}
 
